f1de6b1cd23bcab05484133124ee8ee2810d834a
your-ehsan <ehsanshahid787@gmail.com>
2023-07-31 21:44:05 +0500

    basic example of react query added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const wait = (duration) => {
+    return new Promise((resolve) => setTimeout(resolve, duration));
+  },
+ 
+  App = () => {
+    const PostsQuery = useQuery({
+      queryKey: ["posts"],
+      queryFn: () =>
+        wait(3000).then(async () => {
+          const { data } = await axios.get("http://localhost:3000/posts");
+          return data;
+        }),
+    });
 
-export default App
+    if (PostsQuery.isLoading) {
+      return <h1>Loading ...</h1>;
+    }
+
+    return (
+      <div>
+        <ol type="1">
+          {PostsQuery.data.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  };
+
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const _QueryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={_QueryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+);
